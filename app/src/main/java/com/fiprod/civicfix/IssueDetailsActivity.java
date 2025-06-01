adc5316f0a2880f6fe4828155e611ba36d6e73c7
MD. Famidul Islam Pranto <fipranto9001@gmail.com>
2025-06-01 20:04:06 +0600

    feat(ui) : added basic ui

--- app/src/main/java/com/fiprod/civicfix/IssueDetailsActivity.java
+++ app/src/main/java/com/fiprod/civicfix/IssueDetailsActivity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomeActivity extends AppCompatActivity {
+public class IssueDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_issue_details);
 
     }
 }
